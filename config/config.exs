ede9e788e176148b5f46e418a0496ea12732e4c1
Zach Daniel <zach@zachdaniel.dev>
2022-12-19 20:45:51 -0500

    chore: get git_ops ready

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,17 @@
 import Config
 
 config :tails, :optimize_directions?, true
+
+if Mix.env() == :dev do
+  config :git_ops,
+    mix_project: Tails.MixProject,
+    changelog_file: "CHANGELOG.md",
+    repository_url: "https://github.com/zachdaniel/tails",
+    # Instructs the tool to manage your mix version in your `mix.exs` file
+    # See below for more information
+    manage_mix_version?: true,
+    # Instructs the tool to manage the version in your README.md
+    # Pass in `true` to use `"README.md"` or a string to customize
+    manage_readme_version: "README.md",
+    version_tag_prefix: "v"
+end
--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule Tails.MixProject do
       {:credo, ">= 0.0.0", only: :dev, runtime: false},
       {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
       {:sobelow, ">= 0.0.0", only: :dev, runtime: false},
-      {:git_ops, "~> 2.4.4", only: :dev},
+      {:git_ops, "~> 2.5", only: :dev},
       {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
       {:parse_trans, "3.3.0", only: [:dev, :test], override: true},
       {:elixir_sense, github: "elixir-lsp/elixir_sense", only: [:test, :dev, :docs]}
