91d18dfdcd9040f92ebf5b41e7760de138144bd3
Zach Daniel <zach@zachdaniel.dev>
2022-12-22 02:24:35 -0500

    improvement: support themes being set as strings

--- lib/custom.ex
+++ lib/custom.ex
@@ -367,6 +367,10 @@ defmodule Tails.Custom do
             merge(tailwind, classes(unquote(replacement)))
           end
 
+          def merge_class(%{theme: unquote(to_string(theme))} = tailwind, unquote(to_string(key))) do
+            merge(tailwind, classes(unquote(replacement)))
+          end
+
           if theme == :default do
             # fallback to default
             def merge_class(tailwind, unquote(to_string(key))) do
