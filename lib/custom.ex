418c49dbeebbdf65f15c71ebcb0b09782d8e53c0
Frank Dugan III <frank3@duganator.com>
2023-04-20 14:06:35 -0500

    fix: border-separate typo fixes: #4

--- lib/custom.ex
+++ lib/custom.ex
@@ -135,7 +135,7 @@ defmodule Tails.Custom do
       @table_layouts ~w(auto fixed)
       @mix_blend_modes @blend_modes ++ ~w(plus-lighter)
       @bg_blend_modes @blend_modes
-      @border_collapse_modes ~w(collapse saturate)
+      @border_collapse_modes ~w(collapse separate)
       @transition_timing_functions ~w(ease-linear ease-in ease-out ease-in-out)
       @transition_properties ~w(none all colors opacity shadow transform)
       @animations ~w(none spin ping pulse bounce)
@@ -668,6 +668,8 @@ defmodule Tails.Custom do
           "rounded"
           iex> merge("rounded", "rounded-lg") |> to_string()
           "rounded-lg"
+          iex> merge("border-separate", "border-spacing-1") |> to_string()
+          "border-spacing-1 border-separate"
 
       Classes can be removed
 
